57c71fc27f1e4bbaf451c1cd067fb575acef314c
Amarin Kongthong <amarinkongthong@Amarins-MacBook-Pro.local>
2020-11-24 11:15:08 +0700

    make struct of MegaPokemon class

---
+++ src/MegaPokemon.java
@@ -0,0 +1,9 @@
+public class MegaPokemon extends Pokemon {
+    public MegaPokemon(Pokemon a){
+        super(n, m, t, hp, atk, def, spd);
+    }
+    @Override
+    public PokemonInterface clonePokemon(){
+        
+    }
+}
